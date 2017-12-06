65b473a0e7f66d9bb91b88f104decafb19c26db7
root <root@super-computer>
2017-12-07 01:34:32 +0900

    @

--- mk.bat
+++ mk.bat
@@ -2,6 +2,6 @@ chcp 65001
 
 edub app-dm32.exe run arch=dm32 inc=. app.d def=EnableReal ^
 [my_common-lib#@E:\d-dev\d-sample\common\my_common.lib.bin] ^
-[vibe-d:data] [msgpack-d]
+[vibe-d:data] [my-msgpack-d#@my-msgpack-d]
 if %errorlevel% neq 0 (exit /b)
 ::edub app.lib build arch=dm32 inc=. app.d [botan]
