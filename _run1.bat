b6366c0e21fcedf724187cee3c5ab0e01e75f7e2
root <root@super-computer>
2017-12-07 12:47:45 +0900

    @

--- _run1.bat
+++ _run1.bat
@@ -1,7 +1,9 @@
 /+ 1>nul 2>nul
 @chcp 65001
-copy %~dpnx0 ___tmp.d
-edub ___tmp.exe run ___tmp.d arch=dm32 inc=.
+copy %~dpnx0 tmp___.d
+edub tmp___.exe run tmp___.d arch=dm32 inc=. ^
+def=EnableReal ^
+[vibe-d:data] [my-msgpack-d#@my-msgpack-d]
 @if %errorlevel% neq 0 (exit /b)
 @goto :eof
 +/
