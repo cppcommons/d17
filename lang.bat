77ab352dc5f176201a2f38642775f56dce814eb4
root <root@super-computer>
2017-12-08 17:27:56 +0900

    @

--- lang.bat
+++ lang.bat
@@ -1,4 +1,5 @@
 chcp 65001
-edub lang-ms64.exe run arch=ms64 inc=. lang.d dgrammar.d dparser.d pegged-ms64.lib
+::edub lang-ms64.exe run arch=ms64 inc=. lang.d dgrammar.d dparser.d pegged-ms64.lib
+edub lang-ms64.exe run arch=ms64 lang.d dgrammar.d dparser.d [pegged]
 if %errorlevel% neq 0 (exit /b)
 if exist temp_dparser.d mv temp_dparser.d dparser.d
