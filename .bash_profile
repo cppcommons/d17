3733742e429c0082273a14faa8233d92b333cebf
root <root@super-computer>
2017-12-11 13:05:40 +0900

    @

---
+++ .bash_profile
@@ -0,0 +1,4 @@
+if [ "$HOSTNAME" == "INOT01" ]; then
+    export PATH=/cygdrive/c/Users/Public/D/dmd2/windows/bin:$PATH
+    export PATH=/cygdrive/c/Users/Public/dev1/bin32:$PATH
+fi
---
+++ _script.sh
@@ -0,0 +1,5 @@
+#! bash -uvx
+edub test-script.exe run arch=dm32 script.d runtime.d var2mod.d \
+ [pegged#"~>0.4.2"] [pegged-cutter#"~>1.0.0"] \
+ ["arsd-clone:jsvar"#"~>1.2.2"] ["arsd-clone:script"#"~>1.2.2"] \
+ --build=debug
