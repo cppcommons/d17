f2eabeeac22d5b6717879c898beb673e7bb3c3e5
root <root@super-computer>
2017-12-13 06:06:58 +0900

    @

--- test/simple.bat
+++ test/simple.bat
@@ -2,4 +2,4 @@
 ::E:\d-dev\d17\___ebuild\electron\out\R\electron.exe client.js
 ::node_modules\electron\dist\electron.exe simple.js
 ::call npm install electron ffi ref
-node_modules\electron\dist\electron.exe client.js
+node_modules\electron\dist\electron.exe simple.js
--- test/simple.js
+++ test/simple.js
@@ -1,24 +1,52 @@
-console.log('abc');
 const {app, BrowserWindow} = require('electron')
-
 console.log(process.platform);
 console.log(__dirname);
 
-app.quit()
-/*
-var mainWindows = null
+var ffi = require('ffi');
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-})
+var funcPtr = ffi.Function('void', ['int']);
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+var mylib = ffi.Library('MyDll', {
+  'twice': ['int', ['int']],
+  'doSomething': ['int', [funcPtr, 'int']]
+});
+
+var onResult = function(resultVal) {
+  console.log('Result is', resultVal);
+};
+
+console.log('twice 10 =', mylib.twice(10));
+mylib.doSomething(onResult, 10);
+
+var myExtension = require('./build/Release/my_extension');
+console.log(myExtension);
+console.log(myExtension.hello());
+
+const https = require('https');
+const { URL } = require('url');
+const options = new URL('https://raw.githubusercontent.com/cyginst/cyginst-v1/master/cyginst.bat');
+var img;
+const req = https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.setEncoding('binary');
+  var data = [];
+
+  res.on('data', (d) => {
+    //process.stdout.write(d);
+    data.push( new Buffer( d, 'binary' ) );
+  });
+  res.on('end', function () {
+    console.log('[end]');
+    img = Buffer.concat( data );
+    process.stdout.write(img);
     app.quit()
-  }
-})
-*/
+  });
+});
+req.on('error', (e) => {
+  console.error(e);
+  app.quit()
+});
+req.end();
+
