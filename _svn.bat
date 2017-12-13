beb9ec896b36400a50dae2f48dc9e719cac62a46
root <root@super-computer>
2017-12-14 01:25:57 +0900

    @

--- _svn.bat
+++ _svn.bat
@@ -2,3 +2,4 @@ svn propget git-commit --revprop -r 3 https://github.com/dlang-libs/pegged-cutte
 svn ls -R -v https://github.com/dlang-libs/pegged-cutter/tags/v1.0.1/
 svn ls -R -v https://github.com/dlang-libs/pegged-cutter/trunk
 svn ls -v https://github.com/dlang-libs/pegged-cutter/trunk
+E:\opt\svn\usr\bin\svn.exe propget git-commit --revprop -r 3 https://github.com/dlang-libs/pegged-cutter
