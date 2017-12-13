81a1726eaed472691da7f11032a6d38f0ba472ff
root <root@super-computer>
2017-12-13 21:40:26 +0900

    @

--- ltest.bat
+++ ltest.bat
@@ -1,4 +1,7 @@
 chcp 65001
 g++ -shared -static -o ltest.dll -Ilua-5.3.4/include luatest.cpp -Llua-5.3.4 -llua53
 lua53.exe luatest.lua a b c
+echo local sub=require('ltest'); print(sub.getclip()) | lua53 -
+echo print(#arg) | lua53 - a b c
+echo print( require('ltest').getclip() ) | lua53 -
 ::pause
