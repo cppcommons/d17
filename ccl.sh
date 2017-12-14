8390498579b1ceff8acae5e0f485af966c243bc6
root <root@super-computer>
2017-12-14 14:44:58 +0900

    @

--- ccl.sh
+++ ccl.sh
@@ -1,4 +1,4 @@
 #! bash -uvx
 edub ccl.dll build ccl.d arch=dm32 phobos.lib
 #g++.exe -shared -static -o ccl.dll ccl-cpp.cpp
-echo "" | ccl -b -Q -S 1000000 -l ccl.lisp -- a b c
+echo "" | ccl -b -n -Q -l ccl.lisp -- a b c 漢字
