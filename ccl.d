2540c852efe7cf45af8ddb365ffa547e6c30b34a
root <root@super-computer>
2017-12-14 14:24:56 +0900

    @

--- ccl.d
+++ ccl.d
@@ -1,21 +1,20 @@
-import core.runtime;
+import core.sys.windows.windows;
+import core.sys.windows.winbase;
+
+import core.sys.windows.dll : SimpleDllMain; // file:///C:\D\dmd2\src\druntime\import\core\sys\windows\dll.d
+mixin SimpleDllMain;
 
 extern (C) export int my_add2(int a, int b)
 {
-    Runtime.initialize();
-    /+
     import std.stdio;
-    try {
-        writeln(`a=`, a);
-    } catch(Exception ex)
-    {
-    }
-    +/
-    import core.stdc.stdio; // : printf;
-	freopen("CONIN$", "r", stdin);
-	freopen("CONOUT$", "w", stdout);
-	freopen("CONOUT$", "w", stderr);
-    printf("a=%d\n", a);
-    //writeln(`b=`, b);
+    writeln(`a=`, a);
+    writeln(`b=`, b);
+    /+
+     import core.stdc.stdio; // : printf;
+     freopen("CONIN$", "r", stdin);
+     freopen("CONOUT$", "w", stdout);
+     freopen("CONOUT$", "w", stderr);
+     printf("a=%d\n", a);
+     +/
     return a + b;
 }
